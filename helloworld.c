0197a33a4b3605cdfc864bd83e1d469342a5801b
Ignacy Chorazewicz <ichor001@sledge.cs.ucr.edu>
2016-04-22 10:09:48 -0700

    hello world user program

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include "types.h"
+#include "user.h"
+
+int main(int argc, char** argv) {
+    int pid = fork();
+    if(pid == 0) {
+        printf(1, "Child\n");
+        exit();
+    } else {
+        wait();
+        printf(1, "Parent\n");
+    }
+    exit();
+}
